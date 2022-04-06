92d1205bfe538f45c60281fa216df8936a61b7ea
uyeah0 <s2031@e-mirim.hs.kr>
2022-04-06 22:47:21 +0900

    다중치환 암호키 오류 수정

---
+++ EncryptionKey/src/test.java
@@ -0,0 +1,11 @@
+import java.util.Scanner;
+
+public class test {
+    public static void main(String args[]){
+    	String key = "Abc";
+    	System.out.println(key);
+    	key = key.toLowerCase();
+    	System.out.println(key);
+    	
+    }
+}
